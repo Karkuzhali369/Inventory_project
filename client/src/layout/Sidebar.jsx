2669e3bd5aa914197936dca0780fc62f8277c109
Sailash05 <sailashs2005@gmail.com>
2025-07-27 18:24:49 +0530

    Logout button added in side bar

--- client/src/layout/Sidebar.jsx
+++ client/src/layout/Sidebar.jsx
@@ -11,17 +11,15 @@ const Sidebar = () => {
     };
 
     return (
-        <div>
-            <aside className='backdrop-blur-lg bg-blue-600/90 text-white w-64 h-screen shadow-lg'>
-                <ul className='px-5 py-3 space-y-5'>
-                    <li className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Home</li>
-                    <li className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>View</li>
-                    <li className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Stocks</li>
-                    <li className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Admin Panel</li>
-                    <li onClick={() => handleLogout()} className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Log Out</li>
-                </ul>
-            </aside>
-        </div>
+        <aside className='backdrop-blur-lg bg-blue-600/90 text-white w-64 h-screen shadow-lg'>
+            <ul className='px-5 py-3 space-y-5 flex flex-col h-full'>
+                <li className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Home</li>
+                <li className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>View</li>
+                <li className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Stocks</li>
+                <li className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Admin Panel</li>
+                <li onClick={() => handleLogout()} className='hover:bg-red-700 bg-red-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Log Out</li>
+            </ul>
+        </aside>
     )
 }
 
