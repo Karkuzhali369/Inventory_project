aee226539814ebc8288a48a47a1d8cd18f476976
Sailash05 <sailashs2005@gmail.com>
2025-07-30 13:10:19 +0530

    WIP: Design completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,13 @@
 
-
 const Home = () => {
-  
 
-  return (
-    <div className="text-center p-10">
-      <h1 className="text-2xl font-bold text-green-700 mb-6">
-        Inventory Management System - Main Page
-      </h1>
-
-    </div>
-  );
+    return (
+        <div className="text-center p-10">
+            <h1 className="text-2xl font-bold text-green-700 mb-6">
+                Inventory Management System - Main Page
+            </h1>
+        </div>
+    );
 };
 
 export default Home;
