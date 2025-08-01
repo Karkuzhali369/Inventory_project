e3c5f0ddcfff8a5f077693dc2e3ab68461bf148b
Karkuzhali369 <Karkuzhali303@gmail.com>
2025-08-01 14:45:12 +0530

    wip...

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,17 +9,17 @@ const Home = () => {
             
             <h1 className="text-2xl font-bold text-blue-700 mb-6">
                 Welcome user,</h1>
-                 <div
-        className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24
-    text-4xl sm:text-5xl md:text-6xl
-    bg-red-600 text-white font-bold
-    rounded-full flex items-center justify-center
-    animate-bounce fixed bottom-4 right-4 sm:bottom-6 sm:right-6
-    cursor-pointer z-50"
-        
-        title="show low stock"
-      >
-        !
+        <div
+            className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24
+        text-4xl sm:text-5xl md:text-6xl
+        bg-red-600 text-white font-bold
+        rounded-full flex items-center justify-center
+        animate-bounce fixed bottom-4 right-4 sm:bottom-6 sm:right-6
+        cursor-pointer z-50"
+            
+            title="show low stock"
+        >
+            !
       </div>
 
         </div>
