50d94b074f65ab5a528eb61eccbee17353aed34c
Karkuzhali369 <Karkuzhali303@gmail.com>
2025-08-01 11:56:58 +0530

    home page ...

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,27 @@
+import React from 'react';
 
 const Home = () => {
+     
+
 
     return (
-        <div className="text-center p-10">
-            <h1 className="text-2xl font-bold text-green-700 mb-6">
-                Inventory Management System - Main Page
-            </h1>
+        <div className="text-left p-10">
+            
+            <h1 className="text-2xl font-bold text-blue-700 mb-6">
+                Welcome user,</h1>
+                 <div
+        className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24
+    text-4xl sm:text-5xl md:text-6xl
+    bg-red-600 text-white font-bold
+    rounded-full flex items-center justify-center
+    animate-bounce fixed bottom-4 right-4 sm:bottom-6 sm:right-6
+    cursor-pointer z-50"
+        
+        title="show low stock"
+      >
+        !
+      </div>
+
         </div>
     );
 };
