ac37f9103e07094e30f37b9dfa01f35940cd161e
Sailash05 <sailashs2005@gmail.com>
2025-09-10 20:38:30 +0530

    fix: statistics page removed

--- client/src/layout/Sidebar.jsx
+++ client/src/layout/Sidebar.jsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
                 <Link to='/view' className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>View</Link>
                 <Link to='stock-management' className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Stocks</Link>
                 <Link to='/product-management' className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Product Management</Link>
-                <Link to='/statistics' className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Statistics</Link>
+                {/* <Link to='/statistics' className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Statistics</Link> */}
                 <Link to='/stock-entry' className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Stock Entry Details</Link>
                 <Link to='/stock-added' className='hover:bg-blue-500 cursor-pointer rounded-xl px-4 py-2 transition duration-300'>Stock Added Details</Link>
 
